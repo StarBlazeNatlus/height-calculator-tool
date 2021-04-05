87f807fdefdff21b0de747d58067ea3230ad3a75
Natlus <80473650+StarBlazeNatlus@users.noreply.github.com>
2021-04-05 19:11:07 +0300

    Added v0.0.1

---
+++ script.py
@@ -0,0 +1,26 @@
+#Version 0.0.1, released on 5th of April, 2021
+unit = input ("Enter the unit of measurement used to measure your height: ")
+comp = "Enter your height in "
+colon = ": "
+yas = comp + unit + colon
+height = input (yas)
+exc = "!"
+du = height + unit + exc
+import time
+print ("Your height is")
+time.sleep (1)
+print ("*drumroll*")
+time.sleep (2)
+print (du)
+time.sleep (2)
+print ("Wasn't that amazing?")
+time.sleep (2)
+print ("Weren't you blown away??")
+time.sleep (2)
+print ("Please support the creator!")
+time.sleep (1)
+print ("Open this url pls: https://youtu.be/QKNBsTo-_lI")
+time.sleep (999)
+print ("Oops, you're out of time!")
+time.sleep (5)
+
